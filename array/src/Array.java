bdce9b292ed2865659079d9181d5769a601da885
Liolchik <46924058+Liolchik@users.noreply.github.com>
2020-05-17 21:47:39 +0300

    Lesson2(3)

---
+++ array/src/Array.java
@@ -0,0 +1,9 @@
+public class Array {
+
+    public static void main(String[] args) {
+        int myArray []  = new int [10];
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
