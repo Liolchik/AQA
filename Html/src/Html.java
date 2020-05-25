f2da26298364e93226aeb3b47d8d03a079b7ca7b
Liolchik <46924058+Liolchik@users.noreply.github.com>
2020-05-25 23:44:29 +0300

    Lesson3(3)

---
+++ Html/src/Html.java
@@ -0,0 +1,10 @@
+public class Html {
+    public static void main(String[] fields){
+
+       String pOpen = "<p>";
+       String pClose  = "</p>";
+       String text = "Welcome to Java World";
+       System.out.println(pOpen + text + pClose);
+        }
+    }
+
