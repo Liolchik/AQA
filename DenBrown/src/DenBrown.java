0db4f63b35b8e9a4210482754d8be91dda80a18e
Liolchik <46924058+Liolchik@users.noreply.github.com>
2020-05-25 22:41:31 +0300

    Lesson3(1)

---
+++ DenBrown/src/DenBrown.java
@@ -0,0 +1,9 @@
+public class DenBrown {
+    public static void  main (String [] args){
+
+        String firstName = "Den";
+        String lastName = "Brown";
+
+        System.out.println(firstName + " " + lastName);
+    }
+}
