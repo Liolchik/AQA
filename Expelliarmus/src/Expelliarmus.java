f853b8e1370f6d97172210b572c124f771a6908f
Liolchik <46924058+Liolchik@users.noreply.github.com>
2020-05-25 22:54:30 +0300

    Lesson3(2)

---
+++ Expelliarmus/src/Expelliarmus.java
@@ -0,0 +1,9 @@
+public class Expelliarmus {
+
+    public static void main(String[] args) {
+
+        String expelliarmus = "Expelliarmus";
+        String startSpell = expelliarmus.substring(0,2);
+        System.out.println(startSpell);
+    }
+}
