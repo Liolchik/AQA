10bfaa13c80bd0fd02c5ea75f8ce32762ecb9d53
Liolchik <46924058+Liolchik@users.noreply.github.com>
2020-05-28 22:36:52 +0300

    Lesson3(8)

---
+++ StringEnd/src/StringEnd.java
@@ -0,0 +1,17 @@
+public class StringEnd {
+    public static void main (String [] args){
+
+        String someString = "Here is some string";
+
+        if (someString.endsWith(".")){
+
+            System.out.println(someString);
+        }
+        else {
+
+            System.out.println(someString + ("."));
+        }
+    }
+    }
+
+
