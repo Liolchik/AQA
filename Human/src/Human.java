0ef1de515228a7e42c0108fe720859dab675e9c8
Liolchik <46924058+Liolchik@users.noreply.github.com>
2020-06-14 15:08:25 +0300

    Task(1)

---
+++ Human/src/Human.java
@@ -0,0 +1,18 @@
+public class Human {
+    public static void main(String[] args) {
+        int weight;
+        int age;
+        int hight;
+        String firstName;
+        String lastName;
+
+        public Human (int weight, int  age, int height, String firstName, String lastName ){
+
+            this.weight = weight;
+            this.age = age;
+            this.height = height;
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+    }
+}
