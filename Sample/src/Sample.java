d6bb2db32cb534033d7b93e82eccb7f3676b9bcc
Liolchik <46924058+Liolchik@users.noreply.github.com>
2020-06-14 20:56:16 +0300

    Task(6)

---
+++ Sample/src/Sample.java
@@ -0,0 +1,16 @@
+class Sample {
+
+    int it = 1;
+    String str = "String"
+    double dbl = 123.123;
+
+
+    Sample (int it, String str, double dbl) {
+
+        System.out.println (this.it);
+
+        System.out.println (this.str);
+
+        System.out.println (this.dbl);
+    }
+}
