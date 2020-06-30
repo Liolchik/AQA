f7a13491c9cb1392da79fafa53152381256e320e
Liolchik <46924058+Liolchik@users.noreply.github.com>
2020-06-30 16:46:46 +0300

    Task(5)

--- Packagesss - Copy/src/Main.java
+++
@@ -1,9 +0,0 @@
-public static void main(String[]args){
-
-    public class Main {
-
-        com.pack1.One check1;
-        com.pack2.One check2;
-}
-
-
---
+++ PrivateAccess/src/First.java
@@ -0,0 +1,15 @@
+public static void main(String[]args){
+
+
+public class First {
+    private int one;
+    private String strg;
+}
+
+
+public class Second {
+
+    First obj = new First;
+    System.out.println(obj.strg  + obj.one);
+}
+}
