52b9ee35e792d8f96fada66fe61444524b58bbe4
Liolchik <46924058+Liolchik@users.noreply.github.com>
2020-06-30 18:07:30 +0300

    Task(8)

---
+++ SimpleGetSet/src/SimpleGetSet.java
@@ -0,0 +1,47 @@
+public class SimpleGetSet {
+    int a;
+    int b;
+    int c;
+    int d;
+    int f;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public int getC() {
+        return c;
+    }
+
+    public void setC(int c) {
+        this.c = c;
+    }
+
+    public int getD() {
+        return d;
+    }
+
+    public void setD(int d) {
+        this.d = d;
+    }
+
+    public int getF() {
+        return f;
+    }
+
+    public void setF(int f) {
+        this.f = f;
+    }
+}
