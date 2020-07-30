c7d9914d3d197104aaf4f0c46cd3793efe5dbfbb
Liolchik <46924058+Liolchik@users.noreply.github.com>
2020-07-30 08:06:52 +0300

    Task(7)

---
+++ untitled/src/Example.java
@@ -0,0 +1,31 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Example {
+    private final String first, last;
+
+    public Example(String first, String last) {
+        this.first = first;
+        this.last = last;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || o.getClass() != this.getClass()) return false;
+        Example example = (Example) o;
+
+        return (example.first == this.first && example.last == this.last);
+    }
+
+    @Override
+    public int hashCode() {
+        return 111 * first.hashCode() + last.hashCode();
+    }
+
+    public static void main(String[] args) {
+        Set<Example> s = new HashSet<>();
+        s.add(new Example("Donald", "Duck"));
+        System.out.println(s.contains(new Example("Donald", "Duck")));
+    }
+}
